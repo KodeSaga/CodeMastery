7fc2ea0881bbb8b653db69b0ad4128bee244d76b
KodeSaga <kode.drama@gmail.com>
2018-12-24 23:55:01 +0530

    Update in Day001 program - Trivial solution

--- DailyCodingChallenge/Contents.md
+++ DailyCodingChallenge/Contents.md
@@ -4,4 +4,4 @@
 
 | Date_of_Creation | Code_Folder | Problem_statement |
 | ------- | -------- | ----------------- |
-| 19-Dec-2018 | Day0001 | Given a list of numbers and a number k, return whether any two numbers from the list add up to k. For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17. Bonus: Can you do this in one pass? |
+| 25s-Dec-2018 | Day0001 | Given a list of numbers and a number k, return whether any two numbers from the list add up to k. For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17. Bonus: Can you do this in one pass? |
--- DailyCodingChallenge/Day0001/day0001.c
+++ DailyCodingChallenge/Day0001/day0001.c
@@ -3,6 +3,23 @@
    Language - C
    Problem Statement - Given a list of numbers and a number k, return whether any two numbers from the list add up to k. 
    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17. Bonus: Can you do this in one pass?
+
+
+Rough work :- 
+Trivial Solution:-
+    Step 1 - Sort the array
+    Step 2 - Start with 2 positions i=0 and j=1 .  Add the numbers in these positions and check for k
+    Step 3 - keep doing j+1 till end of the array unless match found(Break and return true and end program)
+    Step 4 - Step 3 no match found, we start from i=i+1 and initializing J to next of i as j=i+1 the repeat step 3
+    Step 5 - return false . End program
+ 
+ I think O(nlogn) for Sorting and O(n^2) for the logic ~ O(n^2)
+
+ 
+
+
+
+
 */
 
 #include <stdio.h>
